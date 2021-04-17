761da7d5eaac8114568322ea93a43a58abd87e1a
Olbrien <77759096+Olbrien@users.noreply.github.com>
2021-04-17 07:20:53 +0100

    Update ft_write.s

--- code_explained/ft_write.s
+++ code_explained/ft_write.s
@@ -9,46 +9,49 @@ ft_write:
 		mov rbp,rsp
 
 		mov rax, 1	; This is the syscall table code for "write".
-					; We will not write the RDI, RSI, RDX because we already have it from
-					; the main.c
-					; https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
+				; We will not write the RDI, RSI, RDX because we already have it from
+				; the main.c
+				; https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
 		syscall		; By typing syscall it goes to the system call 1 (write), and returns
-					; a value for RAX.
-					; If you entered fd = -5 on your main.c your RAX is probably -9
-					; here in Linux.
+				; a value for RAX.
+				; If you entered fd = -5 on your main.c your RAX is probably -9
+				; here in Linux.
 
 		cmp rax, 0	; In Linux the value goes from minus to plus. In Mac it adds a Carry
-					; Flag.
-					; https://docs.freebsd.org/en_US.ISO8859-1/books/developers-handbook/x86-return-values.html
+				; Flag. On MAC you will get a (9) with a Carry Flag. Therefor you don't need
+				; this cmp, and the jl _error bellow it's a jc _error.
+				; https://docs.freebsd.org/en_US.ISO8859-1/books/developers-handbook/x86-return-values.html	
 		jl _error	; If its < 0, jump to error.
 		jmp _return	; Else, return with the value returned from "Write" syscall.
 _error:
 		mov r8, rax			  ; Saves the RAX in R8;
 		call __errno_location ; This calls the function established in "extern".
-							  ; This gets the address of __errno.
-							  ; RAX has the address of __errno. Address is [RAX].
+				      ; This gets the address of __errno.
+				      ; RAX has the address of __errno. Address is [RAX].
 
 
 		neg r8			; If you keep following the example R8 has a value of -9.
-						; But __errno has only positive values. Example: 1 is
-						; /* Operation not permitted */ , 2 is /* No such file or
-						; directory */ etc...
-						; You need to put your -9 positive to correspond to a value in
-						; /sys/errno.h
+					; But __errno has only positive values. Example: 1 is
+					; /* Operation not permitted */ , 2 is /* No such file or
+					; directory */ etc...
+					; You need to put your -9 positive to correspond to a value in
+					; /sys/errno.h
+					; If you are on a MAC you don't need this neg r8, because the value
+					; is positive, on Linux it's a (-9) on MAC it's a (9).
 
 		mov [rax], r8	; You want to change the Address of [RAX].
-						; The address is where you're going to set your '9' for
-						; /* Bad file descriptor */.
-						; To check what it corresponds to, in Linux check:
-						; https://cpip.readthedocs.io/en/latest/_static/dictobject.c/errno.h_85b9b44751f37eef65e30f1b57e3f0fb.html
-						; On a Mac is:
-						; https://unix.superglobalmegacorp.com/Net2/newsrc/sys/errno.h.html
+				; The address is where you're going to set your '9' for
+				; /* Bad file descriptor */.
+				; To check what it corresponds to, in Linux check:
+				; https://cpip.readthedocs.io/en/latest/_static/dictobject.c/errno.h_85b9b44751f37eef65e30f1b57e3f0fb.html
+				; On a Mac is:
+				; https://unix.superglobalmegacorp.com/Net2/newsrc/sys/errno.h.html
 
 		mov rax, -1	; Now that you set the address of [RAX], it's stored there until someone
-					; changes the address. You can change RAX all you want it won't affect
-					; the address you previously changed.
-					; Return -1 is for errors.
-					; I am error.
+				; changes the address. You can change RAX all you want it won't affect
+				; the address you previously changed.
+				; Return -1 is for errors.
+				; I am error.
 		jmp _return
 _return:
 		mov rsp,rbp
